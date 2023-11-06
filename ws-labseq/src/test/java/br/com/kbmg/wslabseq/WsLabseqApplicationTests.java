76a6f20b0a45fd78acbc6bb5865aff62c68f02cd
Kevin Gomes <kb.developer.br@gmail.com>
2023-11-06 20:49:29 +0000

    [ws-labseq] - initial commit

---
+++ ws-labseq/src/test/java/br/com/kbmg/wslabseq/WsLabseqApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kbmg.wslabseq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsLabseqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
