6dcd5c38cb62e86ec61f6d8e09f4ff787541265c
Kevin Gomes <kb.developer.br@gmail.com>
2023-11-07 23:28:24 +0000

    [ws-labseq] - chore(setup): cors configuration

--- ws-labseq/src/main/java/br/com/kbmg/wslabseq/controller/LabSeqController.java
+++ ws-labseq/src/main/java/br/com/kbmg/wslabseq/controller/LabSeqController.java
@@ -5,6 +5,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/labseq")
 @RequiredArgsConstructor
 @Slf4j
+@CrossOrigin("*")
 public class LabSeqController {
 
     private final LabSeqService labSeqService;
